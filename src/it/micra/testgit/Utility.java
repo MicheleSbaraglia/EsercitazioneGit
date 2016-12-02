baad00e13c5b7d2e881a857ea470ae234ee33de3
Michele Sbaraglia <michele.sbaraglia@gmail.com>
2016-12-02 10:39:09 +0100

    8 - Test 1

--- src/it/micra/testgit/Utility.java
+++ src/it/micra/testgit/Utility.java
@@ -2,7 +2,7 @@ package it.micra.testgit;
 
 public class Utility {
 	
-	private static final String TITLE = "STAMPA TITOLO";
+	private static final String TITLE = "Hello World!";
 	
 	
 	public static String getTitle(){
