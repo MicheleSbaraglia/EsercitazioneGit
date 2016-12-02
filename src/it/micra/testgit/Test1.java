c15298f686591ecd5217549e27d28a17d03808b0
Michele Sbaraglia <michele.sbaraglia@gmail.com>
2016-12-02 10:47:54 +0100

    8 - Test 4

--- src/it/micra/testgit/Test1.java
+++ src/it/micra/testgit/Test1.java
@@ -5,7 +5,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		
 		for (int i = 0; i < Utility.getCount(); i++) {
-			System.out.println(Utility.getTitle()+" " + i);
+			System.out.println(Utility.getTitle()+" - " + i);
 		}
 		
 		
