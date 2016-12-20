31b6d09ac42d1f3c658b042861383812691dfff1
Luigi Cecchini <luigi.cecchini@micra.it>
2016-12-20 15:09:06 +0100

    commit

--- src/it/micra/testgit/Test1.java
+++ src/it/micra/testgit/Test1.java
@@ -5,7 +5,11 @@ public class Test1 {
 	public static void main(String[] args) {
 		
 		for (int i = 0; i < Utility.getCount(); i++) {
+<<<<<<< HEAD
 			System.out.println(Utility.getTitle()+" - " + i + "!");
+=======
+			System.out.println(Utility.getTitle()+" - " + i + "!!!!");
+>>>>>>> 2df5519... correzione picocci
 		}
 		
 		
