cb4995e4fc79f6b4773dd30c440e63794a95dc2f
Luigi Cecchini <luigi.cecchini@micra.it>
2017-01-10 18:42:43 +0100

    test commit diff

--- src/it/micra/testgit/Test1.java
+++ src/it/micra/testgit/Test1.java
@@ -1,18 +1,18 @@
 package it.micra.testgit;
 
 public class Test1 {
-	
+
 	public static void main(String[] args) {
-		
+
 		for (int i = 0; i < Utility.getCount(); i++) {
-<<<<<<< HEAD
+
 			System.out.println(Utility.getTitle()+" - " + i + "!");
-=======
+
 			System.out.println(Utility.getTitle()+" - " + i + "!!!!");
->>>>>>> 2df5519... correzione picocci
+
 		}
-		
-		
+
+
 	}
 
 }
