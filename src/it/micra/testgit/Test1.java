bf3b5859867610daa4f4d436b7f24220136195ea
Michele Sbaraglia <michele.sbaraglia@gmail.com>
2017-02-08 10:36:18 +0100

    test year -1

--- src/it/micra/testgit/Test1.java
+++ src/it/micra/testgit/Test1.java
@@ -1,5 +1,10 @@
 package it.micra.testgit;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Test1 {
 
 	public static void main(String[] args) {
@@ -13,6 +18,27 @@ public class Test1 {
 		System.out.println("Valeriottacciabu");
 		System.out.println("ValeriottacciaBis");
 		System.out.println("ValeriottacciaBis3");
+		
+		
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			
+			Date date = sdf.parse("29/02/2016");
+			
+			Calendar c = Calendar.getInstance();
+			System.out.println(sdf.format(c.getTime()));
+			c.setTime(date);
+			System.out.println(sdf.format(c.getTime()));
+			c.add(Calendar.YEAR, -1);
+			
+			
+			
+			System.out.println(sdf.format(c.getTime()));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
 
 	}
 
